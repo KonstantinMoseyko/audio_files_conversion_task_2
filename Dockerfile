486330e49dd3fb86ac57cf655a739a2481523abe
KonstantinMoseyko <mosey577@mail.ru>
2023-06-12 04:19:17 +0300

    NEW | readme docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.10-bullseye
 
+RUN apt-get update && apt-get install -y ffmpeg
+
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
--- core/models/database.py
+++ core/models/database.py
@@ -6,15 +6,15 @@ import os
 
 load_dotenv()
 
-# DATABASE_HOST = os.getenv('DATABASE_HOST')
-# DATABASE_PORT = os.getenv('DATABASE_PORT')
-# DATABASE_NAME = os.getenv('DATABASE_NAME')
-# DATABASE_USER = os.getenv('DATABASE_USER')
-# DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
-# SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-engine = create_engine(f"sqlite:///test.db")
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
